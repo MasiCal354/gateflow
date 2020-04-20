4122a9d5ddbab77123f2136d5db47abcabb6503b
Ubuntu <ubuntu@ip-172-31-23-208.ap-southeast-1.compute.internal>
2020-04-20 10:19:18 +0000

    Complete dataframe.py functionality

--- gateflow/externals/curv/exceptions.py
+++ gateflow/externals/curv/exceptions.py
@@ -1,22 +1,3 @@
-class CurvAPIException(Exception):
-
-    def __init__(self, response):
-        self.code = 0
-        try:
-            json_res = response.json()
-        except ValueError:
-            self.message = 'Invalid JSON error message from Curv: {}'.format(response.text)
-        else:
-            self.code = json_res['code']
-            self.message = json_res['msg']
-        self.status_code = response.status_code
-        self.response = response
-        self.request = getattr(response, 'request', None)
-
-    def __str__(self):  # pragma: no cover
-        return 'APIError(code=%s): %s' % (self.code, self.message)
-
-
 class CurvRequestException(Exception):
     def __init__(self, message):
         self.message = message
